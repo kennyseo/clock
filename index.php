20f7074fca59fd323b49678a0597ba883d9b5922
kennyseo <kenny.seo@websitedevteam.com>
2018-04-03 21:13:40 -0500

    clock

--- index.php
+++ index.php
@@ -15,12 +15,6 @@
 
   <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
   <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
-
-	<link href="css/font.css" rel="stylesheet" type="text/css" media="all">
-	<!--[if gte IE 9]> <link rel="stylesheet" type="text/css" href="css/ie9.css" /> <![endif]-->
-
-
-
 </head>
 
 <body>
@@ -35,7 +29,7 @@
               <div class="collapse navbar-collapse" id="navcol-1">
                   <ul class="nav navbar-nav navbar-right">
                       <li role="presentation"><a href="https://kennyseo.github.io/koreanClock">Home</a></li>
-
+											<li role="presentation"><a href="https://kennyseo.github.io/engClock">Eng. Clock</a></li>
 											<li role="presentation"><a class="navbar-link login" href="https://github.com/kennyseo/koreanClock">Github</a></li>
 											<li role="presentation"><a class="btn btn-default action-button" role="button" href="#download">DOWNLOAD</a></li>
                   </ul>
@@ -57,64 +51,71 @@
 								$hour = date('h', time());
 								$min  = date('i', time());
 
-								echo "<style>.hour, .min {color: #172a74;}";
+								if (class_exists("clock-light")) {
+									$color = "#bb0f33";
+								} else {
+									$color = "#eaeaea";
+								}
+
+
+								echo "<style>.hour, .min {color: " ."". $color ."". ";}";
 								if ($hour == "01") {
-									echo ".one {color: #172a74;}";
+									echo ".one {color: " ."". $color ."". ";}";
 								} elseif ($hour == "02") {
-									echo ".two {color: #172a74;}";
+									echo ".two {color: " ."". $color ."". ";}";
 								} elseif ($hour == "03") {
-									echo ".three {color: #172a74;}";
+									echo ".three {color: " ."". $color ."". ";}";
 								} elseif ($hour == "04") {
-									echo ".four {color: #172a74;}";
+									echo ".four {color: " ."". $color ."". ";}";
 								} elseif ($hour == "05") {
-									echo ".five {color: #172a74;}";
+									echo ".five {color: " ."". $color ."". ";}";
 								} elseif ($hour == "06") {
-									echo ".six {color: #172a74;}";
+									echo ".six {color: " ."". $color ."". ";}";
 								} elseif ($hour == "07") {
-									echo ".seven {color: #172a74;}";
+									echo ".seven {color: " ."". $color ."". ";}";
 								} elseif ($hour == "08") {
-									echo ".eight {color: #172a74;}";
+									echo ".eight1, .eight2 {color: " ."". $color ."". ";}";
 								} elseif ($hour == "09") {
-									echo ".nine1, .nine2 {color: #172a74;}";
+									echo ".nine1, .nine2 {color: " ."". $color ."". ";}";
 								} elseif ($hour == "10") {
-									echo ".ten {color: #172a74;}";
+									echo ".ten {color: " ."". $color ."". ";}";
 								} elseif ($hour == "11") {
-									echo ".ten, .one {color: #172a74;}";
+									echo ".ten, .one {color: " ."". $color ."". ";}";
 								} elseif ($hour == "12") {
-									echo ".ten, .two {color: #172a74;}";
+									echo ".ten, .two {color: " ."". $color ."". ";}";
 								}
 
 
 								// Minutes Starts
 								if ($min{0} == "1") {
-									echo ".tenm {color: #172a74;}";
+									echo ".tenm {color: " ."". $color ."". ";}";
 								} elseif ($min{0} == "2") {
-									echo ".twentym, .tenm {color: #172a74;}";
+									echo ".twentym, .tenm {color: " ."". $color ."". ";}";
 								} elseif ($min{0} == "3") {
-									echo ".thirtym, .tenm {color: #172a74;}";
+									echo ".thirtym, .tenm {color: " ."". $color ."". ";}";
 								} elseif ($min{0} == "4") {
-									echo ".fourtym, .tenm {color: #172a74;}";
+									echo ".fourtym, .tenm {color: " ."". $color ."". ";}";
 								} elseif ($min{0} == "5") {
-									echo ".fiftym, .tenm {color: #172a74;}";
+									echo ".fiftym, .tenm {color: " ."". $color ."". ";}";
 								}
 								if ($min{1} == "1") {
-									echo ".onem {color: #172a74;}";
+									echo ".onem {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "2") {
-									echo ".twom {color: #172a74;}";
+									echo ".twom {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "3") {
-									echo ".threem {color: #172a74;}";
+									echo ".threem {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "4") {
-									echo ".fourm {color: #172a74;}";
+									echo ".fourm {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "5") {
-									echo ".fivem {color: #172a74;}";
+									echo ".fivem {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "6") {
-									echo ".sixm {color: #172a74;}";
+									echo ".sixm {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "7") {
-									echo ".sevenm {color: #172a74;}";
+									echo ".sevenm {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "8") {
-									echo ".eightm {color: #172a74;}";
+									echo ".eightm {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "9") {
-									echo ".nightm {color: #172a74;}";
+									echo ".nightm {color: " ."". $color ."". ";}";
 								} elseif ($min{1} == "0") {
 									echo "";
 								}
@@ -171,10 +172,10 @@
 
 
 	<div id="download" class="container"><!-- skill set container starts -->
-		<h1 class="box mt-5 mb-2 col-md-offset-1 ">EXPERIENCES</h1><!-- EXPERIENCES starts -->
+		<h1 class="box mt-5 mb-2 col-md-offset-1 ">CLOCKS</h1><!-- EXPERIENCES starts -->
 		<div class="row"><!-- row starts -->
-      <div class="col-md-5 col-md-offset-1" style="background: #333;"><!-- col 10 starts -->
-				<table class="clock dark">
+      <div class="col-md-5 col-md-offset-1 dark"><!-- col 10 starts -->
+				<table class="clock-dark">
 					<tr>
 						<td class="ten">열</td>
 						<td class="one">한</td>
@@ -220,8 +221,8 @@
 				</table>
 			</div>
 
-			<div class="col-md-5 col-md-offset-1 ">
-				<table class="clock light">
+			<div class="col-md-5 col-md-offset-1 light">
+				<table class="clock-light">
 					<tr>
 						<td class="ten">열</td>
 						<td class="one">한</td>
@@ -275,8 +276,8 @@
       <div class="col-md-10 col-md-offset-1 "><!-- col 10 starts -->
         <div class="row mb-4">
           <div class="col-md-6">
-            <h1 class="box">ABOUT ME</h1>
-            <h1>Kenny KwanSeok Seo</h1>
+            <h1 class="box">DEVELOPER</h1>
+            <h1>Kenny Seo</h1>
             <h3>Full Stack Developer</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
@@ -286,31 +287,13 @@
             <h3><a href="https://github.com/kennyseo" target="_blank">https://github.com/kennyseo</a></h3>
           </div>
         </div><!-- row ends -->
-          <span class="divider"></span>
       </div><!-- col 10 ends -->
     </div><!-- row ends -->
   </div><!-- about me ends -->
 
 
-
-	<div class="container"><!-- skill set container starts -->
-    <div class="row"><!-- row starts -->
-      <div class="col-md-10 col-md-offset-1 "><!-- col 10 starts -->
-
-					<div class="divider mt-5"></div>
-
-					<h1 class="box mt-5 mb-2">EXPERIENCES</h1><!-- EXPERIENCES starts -->
-			</div>
-		</div>
-	</div>
-
-
-
-
-
-
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
 
